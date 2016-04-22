f0ced0167e0ddf4db314961eb3e78952d4a6e88a
Pawel dziedzic <pawel@mail.com>
2016-04-22 21:59:57 +0200

    c4

---
+++ HackatonPrz2016/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public enum PlayerNumber {PLAYER1, PLAYER2};
+
+	public float moveSpeed = 5;
+	public PlayerNumber number;
+
+	void Update()
+	{
+		string strVert = number == PlayerNumber.PLAYER1 ? "VerticalP1" : "VerticalP2";
+		string strHor = number == PlayerNumber.PLAYER1 ? "HorizontalP1" : "HorizontalP2";
+
+		float vertical = Input.GetAxisRaw (strVert);
+		float horizontal = Input.GetAxisRaw (strHor);
+
+		transform.Translate (new Vector2 (horizontal, vertical) * moveSpeed *  Time.deltaTime);
+	}
+}
