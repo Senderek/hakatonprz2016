ffaa90ba7202ab356c626b1a62a8d936d84dd6ec
Michał Sierpiński <marciel.666@gmail.com>
2016-04-23 15:36:56 +0200

    lalallaa

--- HackatonPrz2016/Assets/Scripts/InGameUIController.cs
+++ HackatonPrz2016/Assets/Scripts/InGameUIController.cs
@@ -1,12 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
 
 public class InGameUIController : MonoBehaviour {
     
         [Header("Nameplates:")]
     public GameObject PlayerName1;
     public GameObject PlayerName2;
+    [Header("Congratz:")]
+    public GameObject WhoWon;
+    public GameObject Finish;
     void Awake() {
+        Finish.SetActive(false);
         if (GameSettings.Player1.name == "")//|| GameSettings.Player1.name == null)
             PlayerName1.GetComponent<UnityEngine.UI.Text>().text = "DWARF1";
         else
@@ -15,5 +20,29 @@ public class InGameUIController : MonoBehaviour {
             PlayerName2.GetComponent<UnityEngine.UI.Text>().text = "DWARF2";
         else
             PlayerName2.GetComponent<UnityEngine.UI.Text>().text = GameSettings.Player2.name.ToString().ToUpper();
+    }
+    public void OnExitPressed()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("mainmenu");
+        
+    }
+    void Start()
+    {
+       StartCoroutine(dontHaveFuckingIdeaWhatItIS());
+    }
+
+    IEnumerator dontHaveFuckingIdeaWhatItIS()
+    {
+        while (true)
+        { 
+            Color color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+            WhoWon.GetComponent<Image>().color = color;
+            yield return new WaitForSeconds(1);
+        }
+    }
+    void Update()
+    {
+      
+        
     }
 }
--- HackatonPrz2016/Assets/Scripts/PlayerHealth.cs
+++ HackatonPrz2016/Assets/Scripts/PlayerHealth.cs
@@ -4,8 +4,9 @@ using System.Collections;
 
 public class PlayerHealth : MonoBehaviour {
     public GameObject HealthBar;
-    
-    
+    public GameObject koniec;
+    public GameObject winner;
+
     public float startingHealth = 100;
 	public GameObject deathParticle;
 
@@ -25,9 +26,12 @@ public class PlayerHealth : MonoBehaviour {
 
         //Debug.Log("1:"+health.ToString());
         //Debug.Log("2: "+HealthBar.GetComponent<UnityEngine.UI.Slider>().value);
-        if(health>0 || HealthBar.GetComponent<UnityEngine.UI.Slider>().value ==0)
-        HealthBar.GetComponent<UnityEngine.UI.Slider>().value=health/100;
+        if (health > 0 || HealthBar.GetComponent<UnityEngine.UI.Slider>().value == 0)
+            HealthBar.GetComponent<UnityEngine.UI.Slider>().value = health / 100;
+        else HealthBar.GetComponent<UnityEngine.UI.Slider>().value = 0;
         //Debug.Log("3:"+HealthBar.GetComponent<UnityEngine.UI.Slider>().value);
+        
+
         try {
 		} catch {
 		}
@@ -46,10 +50,21 @@ public class PlayerHealth : MonoBehaviour {
             HealthBar.GetComponent<UnityEngine.UI.Slider>().value = 0;
             GameObject.FindObjectOfType<CameraController> ().StopUpdating ();
 
-			Instantiate (deathParticle);
-			DestroyImmediate (gameObject);
-		}
+            StartCoroutine(dontHaveFuckingIdeaWhatItIS());
+            Instantiate (deathParticle);
+            mov.enabled = false;
+            GetComponent<AttackController>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+			//DestroyImmediate (gameObject);
+            
+        }
 	}
-
+    IEnumerator dontHaveFuckingIdeaWhatItIS()
+    {
+        yield return new WaitForSeconds(2);
+            koniec.SetActive(true);
+            winner.GetComponent<UnityEngine.UI.Text>().text = "PLAYER " + gameObject.name.ToUpper() + " WON. CONGRATULATIONS";
+        
+    }
 
 }
